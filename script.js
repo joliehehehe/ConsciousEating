b1c7ed92cd35b5d3eb12b47e588bf3dbd8734f1a
joliehehehe <s10205048@connect.np.edu.sg>
2021-02-02 14:37:57 +0800

    Add more navbar info

---
+++ script.js
@@ -0,0 +1,28 @@
+// Countdown page
+var countDownDate = new Date("Feb 24, 2021 16:16:16").getTime();
+
+// To update countdown every 1 second
+var countdownfunction = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Get difference between now and dedicated date an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Output the result in an element with id="demo"
+  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // For when countdown timer is over
+  if (distance < 0) {
+    clearInterval(countdownfunction);
+    document.getElementById("demo").innerHTML = "Lottery Expired";
+  }
+}, 1000);
